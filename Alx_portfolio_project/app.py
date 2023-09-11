f449b3e7a35a84cf76ceceecfd3bae80ccf8f5da
Samuel-Koomson <117752402+Samuel-Koomson@users.noreply.github.com>
2023-09-11 18:29:58 +0000

    Add files via upload
    new updates made including database have been added

--- Alx_portfolio_project/app.py
+++ Alx_portfolio_project/app.py
@@ -1,98 +1,34 @@
-# from flask import Flask, render_template, request
-# from vegg_list import *
-# from alx_portfolio_project import *
-#
-# app = Flask(__name__)
-# app.static_folder = 'static'
-#
-#
-# @app.route('/')
-# def home():
-#     return render_template('home_page.html')
-#
-# @app.route('/select_items', methods=['GET', 'POST'])
-# def select_vegetables():
-#     if request.method == 'POST':
-#         items_needed = request.form.getlist('vegetables')
-#         quantity = int(request.form['quantity'])
-#         shopping_cart = select_items(items_needed, quantity)
-#
-#         shelf_stock, out_of_stock_veggies = check_quantity_in_stock(shopping_cart)
-#         if shelf_stock:
-#             total_cost = calculate_cost(shopping_cart)
-#             return render_template('payment_information.html', shopping_cart=shopping_cart, total_cost=total_cost)
-#         else:
-#             return render_template('out_of_stock.html', out_of_stock_veggies=out_of_stock_veggies)
-#     return render_template('veg_selection', vegetables=vegetables)
-#
-# @app.route('/purchase')
-# def purchase():
-#     if process_payment():
-#         if sufficient_money(50, 100):
-#             purchase_message = "Purchase successful"
-#         else:
-#             purchase_message = "Insufficient funds in your wallet"
-#     else:
-#         purchase_message = "Payment information does not match"
-#     return render_template('purchase_status.html', purchase_message=purchase_message)
-#
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=True, port=8000)
-
-# from flask import Flask, render_template, request, redirect, url_for
-# from vegg_list import *
-# from alx_portfolio_project import *
-#
-# app = Flask(__name__)
-# app.static_folder = 'static'
-# @app.route('/')
-# def home():
-#     return render_template('home_page.html')
-# @app.route('/select_items', methods=['GET', 'POST'])
-# def select_items():
-#     if request.method == 'POST':
-#         items_needed = request.form.getlist('vegetable')
-#         quantities = request.form.getlist('quantity')
-#
-#         total_cost = calculate_cost(items_needed, quantities)
-#         return redirect(url_for('payment-information.html', total_cost=total_cost))
-#
-#     return render_template('vegetable_selection.html', vegetables=vegetables)
-#
-# @app.route('/payment-information', methods=['GET', 'POST'])
-# def payment_information():
-#     if request.method == 'POST':
-#         card_number = request.form.get('card-number')
-#         if process_payment(card_number):
-#             if sufficient_money(50, 100):
-#                 purchase_message = "Purchase successful"
-#             else:
-#                 purchase_message = "Insufficient funds in your wallet"
-#         else:
-#             purchase_message = "Payment information does not match"
-#         return render_template('purchase_status.html', purchase_message=purchase_message)
-#     total_cost=request.args.get('total_cost')
-#     return render_template('payment-information.html', total_cost=total_cost)
-# # @app.route('/purchase', methods=['GET', 'POST'])
-# # def purchase():
-# #     if process_payment():
-# #         if sufficient_money(50, 100):
-# #             purchase_message = "Purchase successful"
-# #         else:
-# #             purchase_message = "Insufficient funds in your wallet"
-# #     else:
-# #         purchase_message = "Payment information does not match"
-# #     return render_template('purchase_status.html', purchase_message=purchase_message)
-#
-#
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8000)
-
-
 from flask import Flask, render_template, request, redirect, url_for
 from vegg_list import vegetables
 from alx_portfolio_project import calculate_cost, check_quantity_in_stock, sufficient_money
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///shop_database.db"
+db = SQLAlchemy(app)
+
+
+class ShoppingCart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vegetable = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+class PaymentInformation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card_number = db.Column(db.Integer, nullable=False)
+    confirm_card = db.Column(db.Integer, nullable=False)
+    card_expiry = db.Column(db.Integer, nullable=False)
+    card_holder = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
 
 # Define a global variable to store the shopping cart
 shopping_cart = []
@@ -116,8 +52,14 @@ def select_items():
 
         # Add the selected items and quantities to the shopping cart
         for veggie, quantity in zip(selected_vegetables, quantities):
+            cart_item = ShoppingCart(
+                vegetable=veggie,
+                quantity=int(quantity)
+            )
+            db.session.add(cart_item)
             shopping_cart.append((veggie, int(quantity)))
 
+        db.session.commit()
         # Redirect back to the vegetable selection page
         return redirect(url_for('view_shopping_cart'))
 
@@ -128,7 +70,8 @@ def select_items():
 # Define a route for the shopping cart page
 @app.route('/shopping_cart')
 def view_shopping_cart():
-    return render_template('shopping_cart.html', shopping_cart=shopping_cart)
+    shopping_cart_items = ShoppingCart.query.all()
+    return render_template('shopping_cart.html', shopping_cart_items=shopping_cart_items)
 
 
 # Define a route for checking the quantity in stock
@@ -137,16 +80,18 @@ def check_quantity():
     shelf_stock, out_of_stock_veggies = check_quantity_in_stock(shopping_cart)
 
     if shelf_stock:
-        return "All items are in stock."
+        return render_template('full_shelf.html', message="All items are in stock.")
     else:
-        return f"Sorry, {out_of_stock_veggies} is out of stock, check back later."
+        return render_template('empty_shelf.html', message=f"Sorry, {out_of_stock_veggies} is out of stock, check back later.")
 
 
 # Define a route for calculating the total cost
 @app.route('/calculate_cost')
 def calculate_cart_cost():
-    total_cost = calculate_cost(shopping_cart)
-    return render_template('calculate_cost.html', total_cost=total_cost)
+    shopping_cart_items = ShoppingCart.query.all()
+
+    total_cost = calculate_cost([(item.vegetable, item.quantity) for item in shopping_cart_items])
+    return render_template('calculate_cost.html', total_cost=f"{total_cost:.2f}")
     # return f"Total cost: ${total_cost:.2f}"
 
 
@@ -154,11 +99,21 @@ def calculate_cart_cost():
 @app.route('/payment', methods=['GET', 'POST'])
 def payment():
     if request.method == 'POST':
-        credit_card_num = int(request.form['card-number'])
+        credit_card_num = request.form['card-number']
+        expiry_date = request.form['expiry-date']
+        cardholder_name = request.form['card-holder-name']
         confirm_credit_card_num = int(request.form['card-confirmation'])
 
         if credit_card_num == confirm_credit_card_num:
-            return redirect(url_for('purchase'))
+            new_card = PaymentInformation(
+                card_number=credit_card_num,
+                card_expiry=expiry_date,
+                card_holder=cardholder_name
+            )
+            db.session.add(new_card)
+            db.session.commit()
+
+        return redirect(url_for('purchase'))
 
     return render_template('payment_information.html')
 
@@ -170,10 +125,11 @@ def purchase():
     wallet_amount = 100  # Assuming a wallet balance of $100
 
     if sufficient_money(total_cost, wallet_amount):
-        return "Purchase Successful"
+        return render_template('purchase_status.html', purchase_message="Purchase Successful")
     else:
-        return "Insufficient funds in your wallet"
+        return render_template('purchase_status.html', purchase_message="Insufficient funds in your wallet")
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
